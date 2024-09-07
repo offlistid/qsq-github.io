1096e493f79c0c738418796efc9a5ad705011068
Offlistid <118656491+offlistid@users.noreply.github.com>
2024-09-07 16:47:11 +0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Сюда вводить данные от БД.
+
+$host = "mysql-de-lim8.joinserver.xyz"; // хост
+$db = "s192867_site"; // название БД
+$login = "u192867_F86H6RwTrd"; // логин
+$pass = "ivOPTuvwVFC^E1nAigu3.u7+"; // пароль
+
+?>
